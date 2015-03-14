b2fc9947223282f7d361ec6812d177b5c40eba67
India Dearlove <india_dearlove@yahoo.co.uk>
2015-03-14 10:09:06 +0000

    trying to get json to work

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,7 +1,15 @@
-$(function() {
+$().ready(
+    function(){
+        var dataSet;
+        $.getJSON("hipstercoffee.js",function(d){}).success(processData);
 
-  $.getJSON('hipstercoffee.json', function(data) {
-  
-  });
+        function processData(ds){
+            dataSet = ds;
+        }
 
-});
+        $("button").click(function(){
+            $("#heading").empty().html(dataSet[Number($(this).attr('val'))].heading);
+            $("#image").empty().html("<img src='" + dataSet[Number($(this).attr('val'))].image + "' />");
+        }
+    }
+);
